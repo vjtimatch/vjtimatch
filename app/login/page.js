39034891940bdb1fb20117570ea13740e3213fac
vjtimatch <vjtimatch@gmail.com>
2025-10-24 12:29:48 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -134,7 +134,7 @@ export default function LoginPage() {
                   )}
                 </Button>
               </Field>
-                  
+
               <p className="text-xs text-center text-muted-foreground mt-2">
               By logging in, you agree to our{' '}
               <a 
@@ -146,7 +146,7 @@ export default function LoginPage() {
                 Terms and Conditions
               </a>.
               </p>
-
+              
               <FieldDescription className="text-center text-sm">
                 Don't have an account?{' '}
                 <a href="/signup" className="underline underline-offset-4 hover:text-primary">
